17825668dd221470918373f97ac85c35839ff5e0
Danilo-Sam <danilobrown17@gmail.com>
2024-01-13 19:30:11 -0300

    Adicionando os testes para adição de comentário

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,29 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('deve adicionar comentario', () =>{
+        render(<Post />)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'eu simplesmente adorei essa miniatura'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('eu simplesmente adorei essa miniatura')).toBeInTheDocument()
+    });
+
+    test('deve adicionar outro comentario', () =>{
+        render(<Post />)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'onde eu consigo compra um desse, é lindo'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('onde eu consigo compra um desse, é lindo')).toBeInTheDocument()
     });
 });
